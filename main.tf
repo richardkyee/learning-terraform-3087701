fab50926e704ffdb59990c8b93c2bf305731bc20
richardkyee <130188803+richardkyee@users.noreply.github.com>
2023-05-15 16:28:58 -0700

    change name of vpc referenced in alb

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ data "aws_ami" "app_ami" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "blog_vpc"
+  name = "dev"
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
@@ -51,8 +51,8 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
+  vpc_id             = module.dev.vpc_id
+  subnets            = module.dev.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
 
