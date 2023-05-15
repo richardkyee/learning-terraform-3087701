87cc3924ea66f6211eb4f4ff3bc9abe8dc937730
richardkyee <130188803+richardkyee@users.noreply.github.com>
2023-05-15 16:47:45 -0700

    rename vpc to blog_vpc

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ module "blog_vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
@@ -50,7 +51,7 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.blog_vpc.vpc_id
+  vpc_id             = module_blog_vpc.vpc_id
   subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
