df582cd2a88829d4b0fca028a1a2c94c6629c903
richardkyee <130188803+richardkyee@users.noreply.github.com>
2023-05-15 16:41:44 -0700

    rename vpc module to blog_vpc

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -50,8 +50,8 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = module.dev.vpc_id
-  subnets            = module.dev.public_subnets
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
   security_groups    = [module.blog_sg.security_group_id]
 
 
